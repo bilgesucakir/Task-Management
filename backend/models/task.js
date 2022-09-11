78670b133e6c7517c301e239994cf6cbdfd24cbb
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2022-09-11 12:13:57 +0300

    Update task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -12,3 +12,4 @@ taskSchema = mongoose.Schema({
 module.exports = mongoose.model('Task', taskSchema); //collection name = tasks
 //if mongoose.model('Name', ...)  then the collection name in the database is 'name'
 //so after connecting to mongosh, entries are searched while in that database as: db.tasks.find()
+//you can find related info about mongosh in your related mongodb atlas cluster page
